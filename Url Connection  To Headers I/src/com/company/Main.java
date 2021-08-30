3ce6e1ba3c73034037fa19b611edaabc413b10ca
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-08-30 08:49:07 +0300

    Add files via upload

---
+++ Url Connection  To Headers I/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            URL url = new URL("http://example.org");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+
+            Map<String , List<String>> headerFields = urlConnection.getHeaderFields();
+            for(Map.Entry<String , List<String>> entry : headerFields.entrySet()){
+                String key = entry.getKey();
+                List<String> value = entry.getValue();
+                System.out.println("-----key = " + key);
+                for(String string : value){
+                    System.out.println("value = " + value);
+                }
+            }
+        }catch (MalformedURLException e){
+            System.out.println("Malformed URL: " + e.getMessage());
+        }catch (IOException e){
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
